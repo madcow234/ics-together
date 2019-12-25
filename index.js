1564c745a36d7eee588fed65877096d3620a6285
MadCow234 <madcow234@gmail.com>
2019-12-25 01:42:22 -0600

    Refactor and add initial timezone display

--- index.js
+++ index.js
@@ -1,69 +1,167 @@
-var app = require("express")();
-var http = require("http").createServer(app);
-var io = require("socket.io")(http);
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const fs = require("fs");
 
-var currentlyTypingList = new Array();
-var currentlyCountingDownList = new Array();
-var numCountdowns = 0;
-var countdownIndex = 0;
+let currentlyTypingList = [];
+let currentlyCountingDownList = [];
+let numCountdowns = 0;
+let countdownIndex = 0;
+
+// Read the timezone files into memory
+let timezonesFile = fs.readFileSync("timezones.json");
+let timezones = JSON.parse(timezonesFile.toString());
+
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   // console.log(req);
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
+
+// Date.prototype.stdTimezoneOffset = function() {
+//   let jan = new Date(this.getFullYear(), 0, 1);
+//   let jul = new Date(this.getFullYear(), 6, 1);
+//   return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+// };
+//
+// Date.prototype.isDstObserved = function() {
+//   return this.getTimezoneOffset() < this.stdTimezoneOffset();
+// };
+
+/**
+ * Calculates the offsets between an hour in UTC and the corresponding timezones at the 4 o'clock hours.
+ *     Ex. utcHour = 4 => return { AM: '+0', PM: '+12' }
+ *     Ex. utcHour = 18 => return { AM: '+10', PM: '-2' }
+ *
+ * @param {Number} utcHour - The hour, in UTC, of which to find the corresponding 4 o'clock timezones
+ * @returns {{AM: string, PM: string}} The offsets between UTC and which timezones correspond to the 4AM and 4PM hours
+ */
+const getICSOffsetStrings = utcHour => {
+  let offsetAM = utcHour < 16 ? 4 - utcHour : 12 - (utcHour % 16);
+  let offsetAMString = offsetAM < 0 ? `${offsetAM}` : `+${offsetAM}`;
+
+  let offsetPM = utcHour < 4 ? -8 - utcHour : 16 - utcHour;
+  let offsetPMString = offsetPM < 0 ? `${offsetPM}` : `+${offsetPM}`;
+
+  return { AM: offsetAMString, PM: offsetPMString };
+};
 
 // Create the ticking clock by emitting the current time on the "clock" channel.
 setInterval(() => {
   let time = new Date();
+  let currentMinute = time.getUTCMinutes();
+  let currentSecond = time.getUTCSeconds();
 
-  let upcoming420Delta = (time.getUTCHours() % 12) - 4;
-  let upcoming420Offset = upcoming420Delta * 3600 * 1000;
+  // The minute of the hour in which to start the ICS warning procedure
+  let target = 60;
+  let alert = target - 1;
+  let warn = target - 2;
 
   let cssClass = "default-clock";
 
-  if (time.getUTCMinutes() === 18 || time.getUTCMinutes() === 19) {
+  if (currentMinute === warn || currentMinute === alert) {
     cssClass = "two-minute-warning";
   }
 
-  if (
-    time.getUTCMinutes() === 19 &&
-    time.getUTCSeconds() >= 30 &&
-    time.getUTCSeconds() <= 59
-  ) {
+  if (currentMinute === alert && currentSecond >= 30 && currentSecond <= 59) {
     cssClass = "thirty-second-alert";
   }
 
-  if (time.getUTCMinutes() === 20) {
-    cssClass = "its-time-success";
+  if (currentMinute === target) {
+    cssClass = "its-time-target";
   }
 
   let clock = {
     time: time,
-    cssClass: cssClass,
-    upcoming420Timezone: `UTC -${upcoming420Delta}`
+    cssClass: cssClass
   };
 
   io.emit("clock", clock);
+
+  // Upcoming timezones and cities are only visible for 20 minutes before an ics
+  if (currentMinute >= target - 20 && currentMinute <= target) {
+    let icsOffsets = getICSOffsetStrings(time.getUTCHours());
+
+    let upcomingTimezones = {
+      AM: [
+        {
+          utcOffset: icsOffsets.AM,
+          timezones: timezones[icsOffsets.AM]
+        }
+      ],
+      PM: [
+        {
+          utcOffset: icsOffsets.PM,
+          timezones: timezones[icsOffsets.PM]
+        }
+      ]
+    };
+
+    if (icsOffsets.AM === "-10") {
+      upcomingTimezones.AM.push({
+        utcOffset: "+14",
+        timezones: timezones["+14"]
+      });
+    }
+
+    if (icsOffsets.AM === "-11") {
+      upcomingTimezones.AM.push({
+        utcOffset: "+13",
+        timezones: timezones["+13"]
+      });
+    }
+
+    if (icsOffsets.PM === "-10") {
+      upcomingTimezones.PM.push({
+        utcOffset: "+14",
+        timezones: timezones["+14"]
+      });
+    }
+
+    if (icsOffsets.PM === "-11") {
+      upcomingTimezones.PM.push({
+        utcOffset: "+13",
+        timezones: timezones["+13"]
+      });
+    }
+
+    io.emit("upcoming-timezones", upcomingTimezones);
+  } else {
+    io.emit("remove-upcoming-timezones");
+  }
 }, 100);
 
+/**
+ * Defines all the listeners to attach to the websocket when a client connects to the server.
+ */
 io.on("connect", socket => {
-  // When a client connects, broadcast a message to other connected clients.
+  /**
+   * When a client connects, broadcast a message to other connected clients.
+   */
   socket.broadcast.emit("connections", "A user connected.");
 
-  // When a client disconnects, broadcast a message to other connected clients.
+  /**
+   * When a client disconnects, broadcast a message to other connected clients.
+   */
   socket.on("disconnect", () => {
     socket.broadcast.emit("connections", "A user disconnected.");
   });
 
-  // When a client sends a message, broadcast it to other connected clients.
-  // This uses the broadcast flag because it is assumed the client that sends the message
-  //   will append it locally instead of waiting for it to be received again over the socket.
+  /**
+   * When a client sends a message, broadcast it to other connected clients.
+   * This uses the broadcast flag because it is assumed the client that sends the message
+   *     will append it locally instead of waiting for it to be received again over the socket.
+   */
   socket.on("messages", messageData => {
     socket.broadcast.emit("messages", messageData);
   });
 
-  // When a client signals that a user has started typing, add that user to the currently
-  //   typing list if it does not already exist, then emit the list to all clients.
+  /**
+   * When a client signals that a user has started typing, add that user to the currently
+   *     typing list if it does not already exist, then emit the list to all clients.
+   */
   socket.on("started_typing", username => {
     if (!currentlyTypingList.includes(username)) {
       currentlyTypingList.push(username);
@@ -71,11 +169,13 @@ io.on("connect", socket => {
     }
   });
 
-  // When a client signals that a user has stopped typing, remove that user from the
-  //   currently typing list, then emit the list to all clients.
+  /**
+   * When a client signals that a user has stopped typing, remove that user from the
+   *     currently typing list, then emit the list to all clients.
+   */
   socket.on("stopped_typing", username => {
     if (currentlyTypingList.includes(username)) {
-      var index = currentlyTypingList.indexOf(username);
+      let index = currentlyTypingList.indexOf(username);
       currentlyTypingList.splice(index, 1);
       io.emit("typing_status", currentlyTypingList);
     }
@@ -87,6 +187,7 @@ io.on("connect", socket => {
     let elementPosX = countdownData.xPosition;
     let elementPosY = countdownData.yPosition;
 
+    // Only allow one countdown per user
     if (currentlyCountingDownList.includes(currentUser)) return;
 
     currentlyCountingDownList.push(currentUser);
@@ -99,6 +200,7 @@ io.on("connect", socket => {
                     background-color:white;
                     position:absolute;
                     padding:0.5vw;
+                    color: black;
                     left:${elementPosX}vw;
                     top:${elementPosY}vh;
                     display:flex;
@@ -109,6 +211,7 @@ io.on("connect", socket => {
       `;
 
     const updateContent = (
+      fontSize,
       divider,
       tMinus,
       three,
@@ -118,124 +221,112 @@ io.on("connect", socket => {
       blastOff
     ) => {
       return `
-        <span style='font-size:1.5vw;
+        <span style='font-size:${fontSize};
                      text-align:center;'>${currentUser}</span>
         <span style='display:${divider ? "block" : "none"};
-                     font-size:1.5vw;
-                     text-align:center;'>----------------</span>
+                     font-size:${fontSize};
+                     text-align:center;'>------------------</span>
         <span style='display:${tMinus ? "block" : "none"};
-                     font-size:1.5vw;
-                     text-align:center;'>T-Minus...</span>
+                     font-size:${fontSize};
+                     text-align:center;'>|.. T-Minus ..|</span>
         <span style='display:${three ? "block" : "none"};
-                     font-size:1.5vw;
-                     text-align:center;'>3...</span>
+                     font-size:${fontSize};
+                     text-align:center;'>|....... 3 .......|</span>
         <span style='display:${two ? "block" : "none"};
-                     font-size:1.5vw;
-                     text-align:center;'>2...</span>
+                     font-size:${fontSize};
+                     text-align:center;'>|....... 2 .......|</span>
         <span style='display:${one ? "block" : "none"};
-                     font-size:1.5vw;
-                     text-align:center;'>1...</span>
+                     font-size:${fontSize};
+                     text-align:center;'>|....... 1 .......|</span>
         <span style='display:${ignition ? "block" : "none"};
-                     font-size:1.5vw;
-                     text-align:center;'>Ignition...</span>
+                     font-size:${fontSize};
+                     text-align:center;'>|.. Ignition ..|</span>
         <span style='display:${blastOff ? "block" : "none"};
-                     font-size:1.5vw;
-                     text-align:center;'>Blast Off!!!</span>
+                     font-size:${fontSize};
+                     text-align:center;'>| Blast off!! |</span>
       `;
     };
 
-    let countdown = {
-      id: `${elementId}`,
-      container: createContainer,
-      content: null,
-      action: "create"
-    };
-    io.emit("run_countdown", countdown);
+    // Trigger the client to append a countdown container to the page
+    io.emit("start_countdown", createContainer);
 
     numCountdowns++;
 
     setTimeout(() => {
-      countdown = {
+      let countdown = {
         id: `${elementId}`,
         container: createContainer,
-        content: updateContent(true, false, false, false, false, false, false),
-        action: "update"
+        content: updateContent('1.5vw', 1, 0, 0, 0, 0, 0, 0)
       };
-      io.emit("run_countdown", countdown);
+      io.emit("update_countdown", countdown);
     }, 2000);
 
     setTimeout(() => {
-      countdown = {
+      let countdown = {
         id: `${elementId}`,
         container: createContainer,
-        content: updateContent(true, true, false, false, false, false, false),
-        action: "update"
+        content: updateContent('1.5vw', 1, 1, 0, 0, 0, 0, 0)
       };
-      io.emit("run_countdown", countdown);
+      io.emit("update_countdown", countdown);
     }, 3000);
 
     setTimeout(() => {
-      countdown = {
+      let countdown = {
         id: `${elementId}`,
         container: createContainer,
-        content: updateContent(true, true, true, false, false, false, false),
-        action: "update"
+        content: updateContent('1.4vw', 1, 1, 1, 0, 0, 0, 0)
       };
-      io.emit("run_countdown", countdown);
+      io.emit("update_countdown", countdown);
     }, 4000);
 
     setTimeout(() => {
-      countdown = {
+      let countdown = {
         id: `${elementId}`,
         container: createContainer,
-        content: updateContent(true, true, true, true, false, false, false),
-        action: "update"
+        content: updateContent('1.3vw', 1, 1, 1, 1, 0, 0, 0)
       };
-      io.emit("run_countdown", countdown);
+      io.emit("update_countdown", countdown);
     }, 5000);
 
     setTimeout(() => {
-      countdown = {
+      let countdown = {
         id: `${elementId}`,
         container: createContainer,
-        content: updateContent(true, true, true, true, true, false, false),
-        action: "update"
+        content: updateContent('1.2vw', 1, 1, 1, 1, 1, 0, 0)
       };
-      io.emit("run_countdown", countdown);
+      io.emit("update_countdown", countdown);
     }, 6000);
 
     setTimeout(() => {
-      countdown = {
+      let countdown = {
         id: `${elementId}`,
         container: createContainer,
-        content: updateContent(true, true, true, true, true, true, false),
-        action: "update"
+        content: updateContent('1.0vw', 1, 1, 1, 1, 1, 1, 0)
       };
-      io.emit("run_countdown", countdown);
+      io.emit("update_countdown", countdown);
     }, 7000);
 
     setTimeout(() => {
-      countdown = {
+      let countdown = {
         id: `${elementId}`,
         container: createContainer,
-        content: updateContent(true, true, true, true, true, true, true),
-        action: "update"
+        content: updateContent('0.9vw', 1, 1, 1, 1, 1, 1, 1)
       };
-      io.emit("run_countdown", countdown);
+      io.emit("update_countdown", countdown);
     }, 8000);
 
     setTimeout(() => {
-      countdown = {
-        id: `${elementId}`,
-        container: null,
-        content: null,
-        action: "remove"
-      };
-      io.emit("run_countdown", countdown);
+      // Trigger the client to remove the countdown when it finishes
+      io.emit("stop_countdown", elementId);
+
+      // Remove the user from the currently counting down list
+      currentlyCountingDownList.splice(
+        currentlyCountingDownList.indexOf(currentUser)
+      );
+
       numCountdowns--;
+      // Reset the countdown index if no countdowns are on the screen
       if (numCountdowns === 0) countdownIndex = 0;
-      let usernameIndex = currentlyCountingDownList.indexOf(currentUser);
-      currentlyCountingDownList.pop(usernameIndex);
     }, 10000);
 
     countdownIndex++;
---
+++ public/js/ics.js
@@ -0,0 +1,299 @@
+$(function() {
+  let socket = io();
+  let typingList = [];
+  let timezones = null;
+
+  // The server will update the clock 10 times per second, but we don't want
+  //   jQuery constantly updating the DOM, so always check to see if the
+  //   client actually needs to be updated before invoking jQuery functions.
+  socket.on("clock", newClock => {
+    let $clock = $("#clock");
+    let $clockContainer = $("#clock-container");
+
+    let currentTime = new Date(newClock.time).toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      hour12: true
+    });
+
+    // Update the clock time if it changes
+    if ($clock.html() !== currentTime) {
+      $clock.html(currentTime);
+    }
+
+    // Update the CSS class if it changes
+    if (!$clockContainer.hasClass(newClock.cssClass)) {
+      $clockContainer.removeClass();
+      $clockContainer.addClass(newClock.cssClass);
+    }
+  });
+
+  socket.on("upcoming-timezones", upcomingTimezones => {
+    let $upcomingContainer = $("#upcoming-container");
+    if ($upcomingContainer.css("display") !== "flex") {
+      $upcomingContainer.css({ display: "flex" });
+    }
+
+    if (JSON.stringify(upcomingTimezones) !== JSON.stringify(timezones)) {
+      timezones = null;
+      timezones = upcomingTimezones;
+
+      let amOffset = upcomingTimezones.AM[0].utcOffset;
+      if (upcomingTimezones.AM.length === 2) {
+        amOffset += "/" + upcomingTimezones.AM[1].utcOffset;
+      }
+
+      let pmOffset = upcomingTimezones.PM[0].utcOffset;
+      if (upcomingTimezones.PM.length === 2) {
+        pmOffset += "/" + upcomingTimezones.PM[1].utcOffset;
+      }
+
+      $("#am-offset").html(`AM: UTC ${amOffset}`);
+      $("#pm-offset").html(`PM: UTC ${pmOffset}`);
+
+      let amInterval = upcomingTimezones.AM[0].timezones.length;
+      if (upcomingTimezones.AM.length === 2) {
+        amInterval += upcomingTimezones.AM[1].timezones.length;
+      }
+
+      let pmInterval = upcomingTimezones.PM[0].timezones.length;
+      if (upcomingTimezones.PM.length === 2) {
+        pmInterval += upcomingTimezones.PM[1].timezones.length;
+      }
+
+      const loopTimezonesAM = () => {
+        for (let i = 0; i < upcomingTimezones.AM.length; i++) {
+          let numTimezones = upcomingTimezones.AM[0].timezones.length;
+          setTimeout(() => {
+            numTimezones = upcomingTimezones.AM[i].timezones.length;
+            let amTimezone = upcomingTimezones.AM[i];
+            for (let j = 0; j < amTimezone.timezones.length; j++) {
+              setTimeout(() => {
+                let timezone = amTimezone.timezones[j];
+                $("#am-timezone").html(
+                  `<div class="timezone">${timezone.name}</div>`
+                );
+
+                $("#am-cities-slider").html("");
+                timezone['cities'].forEach(city => {
+                  $("#am-cities-slider").append(`
+                    <div class="city">
+                      <span>${city.city}</span>
+                      <span>${city.state}</span>
+                      <span>${city.country}</span>
+                    </div>`);
+                });
+              }, 5000 * j);
+            }
+          }, 5000 * i * numTimezones);
+        }
+      };
+
+      const loopTimezonesPM = () => {
+        for (let i = 0; i < upcomingTimezones.PM.length; i++) {
+          let numTimezones = upcomingTimezones.PM[0].timezones.length;
+          setTimeout(() => {
+            numTimezones = upcomingTimezones.PM[i].timezones.length;
+            let pmTimezone = upcomingTimezones.PM[i];
+            for (let j = 0; j < pmTimezone.timezones.length; j++) {
+              setTimeout(() => {
+                let timezone = pmTimezone.timezones[j];
+                $("#pm-timezone").html(
+                  `<div class="timezone">${timezone.name}</div>`
+                );
+
+                $("#pm-cities-slider").html("");
+                timezone['cities'].forEach(city => {
+                  $("#pm-cities-slider").append(`
+                    <div class="city">
+                      <span>${city.city}</span>
+                      <span>${city.state}</span>
+                      <span>${city.country}</span>
+                    </div>`);
+                });
+              }, 5000 * j);
+            }
+          }, 5000 * i * numTimezones);
+        }
+      };
+
+      loopTimezonesAM();
+      loopTimezonesPM();
+
+      setInterval(() => {
+        loopTimezonesAM();
+      }, 5000 * amInterval);
+
+      setInterval(() => {
+        loopTimezonesPM();
+      }, 5000 * pmInterval);
+
+      // timezones.AM[0].timezones.forEach(timezone => {
+      //   timezone.cities.forEach(city => {
+      //     $("#am-cities-slider").append(`
+      //       <div class="city">
+      //         <span>${city.city}</span>
+      //         <span>${city.state}</span>
+      //         <span>${city.country}</span>
+      //       </div>`);
+      //   });
+      // });
+
+      // timezones.PM[0].timezones.forEach(timezone => {
+      //   timezone.cities.forEach(city => {
+      //     $("#pm-cities-slider").append(`
+      //       <div class="city">
+      //         <span>${city.city}</span>
+      //         <span>${city.state}</span>
+      //         <span>${city.country}</span>
+      //       </div>`);
+      //   });
+      // });
+
+      // let amSliderWidth = $("#am-cities-slider").width();
+      // let pmSliderWidth = $("#pm-cities-slider").width();
+
+      // $("#am-cities-slider")
+      //   .get(0)
+      //   .style.setProperty("--slider-width", `-${amSliderWidth}px`);
+      // $("#pm-cities-slider")
+      //   .get(0)
+      //   .style.setProperty("--slider-width", `-${pmSliderWidth}px`);
+
+      // $("#am-cities-slider")
+      //   .get(0)
+      //   .style.setProperty("animation", `slide infinite 30s linear`);
+      // $("#pm-cities-slider")
+      //   .get(0)
+      //   .style.setProperty("animation", `slide infinite 30s linear`);
+    }
+  });
+
+  socket.on("remove-upcoming-timezones", () => {
+    let $upcomingContainer = $("#upcoming-container");
+    if ($upcomingContainer.css("display") !== "none") {
+      $upcomingContainer.css({ display: "none" });
+    }
+  });
+
+  $("#messages").append($("<li>").text("You are connected."));
+
+  $("#clock-container").on("click", e => {
+    // Require a shift-click to start a countdown
+    if (e.shiftKey) {
+      // Prevent starting a countdown over top of an existing countdown
+      if (
+        e.target.id === "upcoming-timezone" ||
+        e.target.id === "upcoming-cities" ||
+        e.target.id === "clock-container" ||
+        e.target.id === "clock"
+      ) {
+        let $username = $("#username");
+        if ($username.val() !== "") {
+          let xPos = (e.clientX / $(document).width()) * 100;
+          let yPos = (e.clientY / $(document).height()) * 100;
+          let countdownData = {
+            username: $username.val(),
+            xPosition: xPos,
+            yPosition: yPos
+          };
+          socket.emit("initiate_countdown", countdownData);
+        }
+      }
+    }
+  });
+
+  socket.on("start_countdown", countdownContainer => {
+    $("#clock-container").append(countdownContainer);
+  });
+
+  socket.on("update_countdown", countdown => {
+    // Append the countdown container to the page if it isn't already there
+    // This is to sync up when a client joins during an active countdown
+    let $countdownId = $(`#${countdown.id}`);
+    if (!$countdownId.length) {
+      $("#clock-container").append(countdown.container);
+    }
+    $countdownId.html(countdown.content);
+  });
+
+  socket.on("stop_countdown", countdownId => {
+    $(`#${countdownId}`).remove();
+  });
+
+  $("form").on("submit", e => {
+    e.preventDefault();
+    let $message = $("#message");
+    let messageData = {
+      username: $("#username").val() || "(anonymous)",
+      message: $message.val()
+    };
+    if (messageData.message !== "") {
+      socket.emit("messages", messageData);
+      $("#messages").append(
+        $("<li>").text(messageData.username + ": " + messageData.message)
+      );
+      scrollToBottom();
+      $message.val("");
+    }
+    socket.emit("stopped_typing", messageData.username);
+    return false;
+  });
+
+  $("#message").on("input", () => {
+    let $message = $("#message");
+    let username = $("#username").val() || "(anonymous)";
+    if ($message.val() === "") {
+      socket.emit("stopped_typing", username);
+    } else {
+      if (!typingList.includes(username)) {
+        socket.emit("started_typing", username);
+      }
+    }
+
+    let prevMessage = $message.val();
+    setTimeout(() => {
+      if (prevMessage === $message.val()) {
+        socket.emit("stopped_typing", username);
+      }
+    }, 3000);
+  });
+
+  socket.on("messages", data => {
+    $("#messages").append($("<li>").text(data.username + ": " + data.message));
+    scrollToBottom();
+  });
+
+  socket.on("connections", msg => {
+    $("#messages").append($("<li>").text(msg));
+    scrollToBottom();
+  });
+
+  socket.on("typing_status", currentlyTypingList => {
+    let $typingStatus = $("#typing-status");
+    typingList = currentlyTypingList;
+    $("#typing-status-message").remove();
+    if (currentlyTypingList.length === 1) {
+      $typingStatus.append(
+        '<div id="typing-status-message">' +
+          currentlyTypingList +
+          " is typing...</div>"
+      );
+    } else if (currentlyTypingList.length > 1) {
+      $typingStatus.append(
+        '<div id="typing-status-message">' +
+          currentlyTypingList.join(", ") +
+          " are typing...</div>"
+      );
+    }
+  });
+
+  const scrollToBottom = () => {
+    $("#messages-container").scrollTop($("#messages").height());
+  };
+
+  // const sleep = ms => {
+  //   return new Promise(resolve => setTimeout(resolve, ms));
+  // };
+});
